31e79a210ffcc4b0df7e8ad3e79407d6a25c8e01
zhx680 <1094567680@qq.com>
2019-06-04 11:06:48 +0800

    更新版本控制文件

---
+++ Algorithm/array/src/com/company/algorithm/Main.java
@@ -0,0 +1,21 @@
+package com.company.algorithm;
+
+
+
+
+public class Main {
+    public class Student{
+       public Student(){
+
+       }
+
+    }
+
+    public static void main(String[] args) {
+	// write your code here'
+        System.out.println("hello");
+        int[] arr=new int[10];
+
+    }
+}
+
